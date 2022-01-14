b4d5455e55fec4a58dda16a45cc292a6dbc605ae
ehretlo <alss-sio-sisr21-elo@ccicampus.fr>
2022-01-14 17:21:11 +0100

    ajout des sources de mes fonds 1 et 2

---
+++ HMTL CCI-SIO-21-Portfolio/Portfolio JS.js
@@ -0,0 +1,3 @@
+let titreH1 = document.getElementById("monTitreH1");
+alert(titreH1.textContent); // Affichage avec une boite de dialogue
+console.log(titreH1.textContent); // Affichage sur la consol
